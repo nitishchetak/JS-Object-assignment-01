1a4731cd43cdb8f12f67bcf0053f1f804f467ff4
nitishchetak <91657439+nitishchetak@users.noreply.github.com>
2021-11-26 19:25:46 +0530

    Add files via upload

---
+++ Object.js
@@ -0,0 +1,27 @@
+var person ={
+ firstName: "Nitish",
+ lastName: "kumar",
+ officeAdress: "Gola road,Gaya"
+};
+console.log("person1");
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.officeAdress);
+var person ={
+ firstName: "Mohan",
+ lastName: "Singh",
+ officeAdress: "Gandhi Maidan,Patna"
+};
+console.log("person2");
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.officeAdress);
+var person ={
+ firstName: "Ankit",
+ lastName: "kumar",
+ officeAdress: "Boring Road,Patna"
+};
+console.log("person3");
+console.log(person.firstName);
+console.log(person.lastName);
+console.log(person.officeAdress);
